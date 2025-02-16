0c796b8501a4fe3347fe38c8ed145904127a816b
agolovanov <alexandergolovanovvla@gmail.com>
2025-02-16 11:24:37 +0300

    .

--- users/apps.py
+++ users/apps.py
@@ -1,24 +1,36 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import Group, Permission
 
 class UsersConfig(AppConfig):
     name = 'users'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
-        from django.db.models.signals import post_migrate
-        default_auto_field = 'django.db.models.BigAutoField'
         post_migrate.connect(create_groups, sender=self)
 
 def create_groups(sender, **kwargs):
-    from django.contrib.auth.models import Group
-    user_group, created = Group.objects.get_or_create(name='User')
-    manager_group, created = Group.objects.get_or_create(name='Manager')
-    
-    if created:
-        user_group.permissions.set([
-            'add_mailing', 'change_mailing', 'view_mailing',
-            'add_receiver', 'change_receiver', 'view_receiver'
-        ])
-        manager_group.permissions.set([
-            'view_user', 'view_mailing', 'view_receiver',
-            'change_mailing', 'change_receiver'
-        ])
+    """Создаём группы и назначаем им права после миграций"""
+    user_group, _ = Group.objects.get_or_create(name='User')
+    manager_group, _ = Group.objects.get_or_create(name='Manager')
+
+    # Получаем объекты Permission
+    user_permissions = [
+        Permission.objects.get(codename='add_mailing', content_type__app_label='mailings'),
+        Permission.objects.get(codename='change_mailing', content_type__app_label='mailings'),
+        Permission.objects.get(codename='view_mailing', content_type__app_label='mailings'),
+        Permission.objects.get(codename='add_receiver', content_type__app_label='mailings'),
+        Permission.objects.get(codename='change_receiver', content_type__app_label='mailings'),
+        Permission.objects.get(codename='view_receiver', content_type__app_label='mailings'),
+    ]
+
+    manager_permissions = [
+        Permission.objects.get(codename='view_user', content_type__app_label='users'),
+        Permission.objects.get(codename='view_mailing', content_type__app_label='mailings'),
+        Permission.objects.get(codename='view_receiver', content_type__app_label='mailings'),
+        Permission.objects.get(codename='change_mailing', content_type__app_label='mailings'),
+        Permission.objects.get(codename='change_receiver', content_type__app_label='mailings'),
+    ]
+
+    user_group.permissions.set(user_permissions)
+    manager_group.permissions.set(manager_permissions)
